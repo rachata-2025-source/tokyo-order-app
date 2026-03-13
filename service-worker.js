c01090ff471de241b82f8730b44472e7a9bcdc5a
rachata-2025-source <rachata.ta2024@gmail.com>
2026-03-13 10:45:02 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(
-    caches.open('tokyo-v1').then(c=>
-      c.addAll(['./','index.html'])
-    )
-  );
+const CACHE_NAME = 'makin-v3.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600&display=swap',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
-  );
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
